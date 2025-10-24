aca513856d0698890701562422bb8a09eabaa5c8
panyxy <yxpan.cv@gmail.com>
2025-10-24 12:36:20 +0800

    initial code

--- scripts/test_vrplib_script.sh
+++ scripts/test_vrplib_script.sh
@@ -20,9 +20,9 @@ CUDA_VISIBLE_DEVICES=0 python test_vrplib.py \
 CUDA_VISIBLE_DEVICES=0 python test_vrplib.py \
 --log_path $PROJECT_ROOT/logs \
 --dataset_path $PROJECT_ROOT/data/vrplib \
---checkpoint $PROJECT_ROOT/pretrained_moses_model/cada/100/multilora_VR1route_softplus.ckpt \
+--checkpoint $PROJECT_ROOT/pretrained_moses_model/cada/100/multilora_VR1route_sigmoid.ckpt \
 --model_name cada_multilora \
---lora_act_func softplus \
+--lora_act_func sigmoid \
 --lora_use_trainable_layer 1 \
 --lora_use_dynamic_topK 1 \
 --lora_use_basis_variants 0 \
